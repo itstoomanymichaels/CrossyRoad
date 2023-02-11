74e09e0d4f3e7530799f23a3700618756b5c86a2
itstoomanymichaels <maikeruh03@gmail.com>
2023-02-11 09:25:35 -0500

    Refactored MainActivity to Welcome, style added to Welcome, and button functionality added to proceed to config screen

---
+++ app/src/main/java/com/example/crossyroad/Welcome.java
@@ -0,0 +1,26 @@
+package com.example.crossyroad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        Button start = (Button) findViewById(R.id.startButton);
+
+    }
+
+
+    public void goConfigScreen(View view) {
+        //Intent configScreen = new Intent(this, configscreen.class));
+        //startActivity(configScreen);
+    }
+}
