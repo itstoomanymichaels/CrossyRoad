5842ea67461e84e90181ee8a80d8c0a4135b1a43
amishra343 <amishra343@gatech.edu>
2023-02-13 18:08:12 -0500

    Implemented selected character sprite shown in game screen.

--- app/src/main/java/com/example/crossyroad/GameScreen.java
+++ app/src/main/java/com/example/crossyroad/GameScreen.java
@@ -3,7 +3,6 @@ package com.example.crossyroad;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
