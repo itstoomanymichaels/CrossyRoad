685b5493bc4309372d0b60adc51e4781acd85c8a
Mansour, Ashraf Mahmoud <amansour32@gatech.edu>
2023-04-02 20:01:10 -0400

    Create Sprint4_RespawnTest.java
    Added RespawnTest

---
+++ app/src/androidTest/java/com/example/crossyroad/Sprint4_RespawnTest.java
@@ -0,0 +1,137 @@
+package com.example.crossyroad;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import android.util.Log;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class Sprint4_RespawnTest {
+
+    // driving logic to test
+    public int drive(int x, int speed, int width, int screenX, String direction) {
+        if (direction.equals("L")) {
+            x -= speed;
+            if ((x + width) <= 0) {
+                x = screenX + screenX / 20;
+            }
+        } else {
+            x += speed;
+            if (x >= screenX) {
+                x = -screenX / 20 - width;
+            }
+        }
+        return x;
+    }
+
+    @Test
+    public void testCar() {
+
+        int speed = 5;
+        int width = 10;
+        int screenX = 400;
+        int xPos = (5 - 4) * 8 * screenX / 20;
+
+        for (int i = 0; i < 1000; i++) { // run 1000 frames
+            xPos = drive(xPos, speed, width, screenX, "R");
+        }
+
+        if (xPos < screenX) { // check if vehicle is still on screen
+            assert(true);
+        }
+        else {
+            assert(false);
+        }
+
+        //test other direction
+        xPos = 3 * 6 * screenX / 20;
+        for (int i = 0; i < 1000; i++) { // run 1000 frames
+            xPos = drive(xPos, speed, width, screenX, "L");
+        }
+
+        if ((xPos + width) > 0) { // check if vehicle is still on screen
+            assert(true);
+        }
+        else {
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testBus() {
+
+        int speed = 10;
+        int width = 20;
+        int screenX = 400;
+        int xPos = (18 - 16) * 10 * screenX / 20;
+
+        for (int i = 0; i < 1000; i++) { // run 1000 frames
+            xPos = drive(xPos, speed, width, screenX, "R");
+        }
+
+        if (xPos < screenX) { // check if vehicle is still on screen
+            assert(true);
+        }
+        else {
+            assert(false);
+        }
+
+        //test other direction
+        xPos = (14 - 13) * 10 * screenX / 20;
+        for (int i = 0; i < 1000; i++) { // run 1000 frames
+            xPos = drive(xPos, speed, width, screenX, "L");
+        }
+
+        if ((xPos + width) > 0) { // check if vehicle is still on screen
+            assert(true);
+        }
+        else {
+            assert(false);
+        }
+    }
+
+    @Test
+    public void testTruck() {
+
+        int speed = 10;
+        int width = 20;
+        int screenX = 400;
+        int xPos = (11 - 10) * 10 * screenX / 20;
+
+        for (int i = 0; i < 1000; i++) { // run 1000 frames
+            xPos = drive(xPos, speed, width, screenX, "R");
+        }
+
+        if (xPos < screenX) { // check if vehicle is still on screen
+            assert(true);
+        }
+        else {
+            assert(false);
+        }
+
+        //test other direction
+        xPos = (15 - 13) * 10 * screenX / 20;
+        for (int i = 0; i < 1000; i++) { // run 1000 frames
+            xPos = drive(xPos, speed, width, screenX, "L");
+        }
+
+        if ((xPos + width) > 0) { // check if vehicle is still on screen
+            assert(true);
+        }
+        else {
+            assert(false);
+        }
+    }
+}
