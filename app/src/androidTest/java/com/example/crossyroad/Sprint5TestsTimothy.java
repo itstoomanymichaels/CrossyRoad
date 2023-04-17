dd3281c0634e586da8a374ef844088cdddbca5b0
mherndon32 <mherndon32@gatech.edu>
2023-04-16 22:24:51 -0400

    Amended checkstyle issues

--- app/src/androidTest/java/com/example/crossyroad/Sprint5TestsTimothy.java
+++ app/src/androidTest/java/com/example/crossyroad/Sprint5TestsTimothy.java
@@ -8,43 +8,35 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.*;
-import android.content.res.Resources;
-import android.graphics.Rect;
 import android.widget.TextView;
 
 import static org.mockito.Mockito.mock;
 
 
+@RunWith(AndroidJUnit4.class)
 public class Sprint5TestsTimothy {
 
     private GameWin gw;
     private TextView congratsText;
-    private Log log = new BranchLog(0, 0, 200, 100, 14, "L",
-            Resources.getSystem());
-    private Frog frog = new Frog(0,0, Resources.getSystem(), 1);
-    Rect f = new Rect(frog.getX(), frog.getY(), frog.getX() + frog.getWidth(),
-            frog.getY() + frog.getHeight());
-    Rect c = new Rect(log.getX(), log.getY(), log.getX() + log.getWidth(), log.getY() + log.getHeight());
-
+    private Log log;
+    private Frog frog;
     @Before
-
     public void setUp() {
         gw = mock(GameWin.class);
         congratsText = mock(TextView.class);
+        log = mock(BranchLog.class);
+        frog = mock(Frog.class);
     }
 
     @Test
-
     public void congratsShown() {
         Mockito.when(gw.getCongrats()).thenReturn(congratsText);
         assertTrue(gw.getCongrats() != null);
     }
 
     @Test
-
     public void getOnLog() {
         Mockito.when(log.isCollided(frog)).thenReturn(true);
         assertTrue(log.isCollided(frog));
     }
-
 }
